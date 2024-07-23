11a3a020d0a98ca7f36bdafc0b7b2480b5d0085a
PRANJAY KUMAR <pranjayku123@gmail.com>
2024-07-23 14:55:19 +0530

    hook

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react';
+import  { useState,} from 'react';
 import Test from './test';
 function App() {
-  const [count, setCount] = useState(0);
-
+  let [counter, setCounter] = useState(15)
+  const addValue = () => {
+    if (counter < 20) {
+      setCounter(counter + 1);
+    }
+  } 
+  const removeValue = () => {  
+    if (counter > 0) {
+      setCounter(counter - 1);
+    }
+  }
   return (
     <>
-    <Test />
-    <h5>Pranjay</h5>
+    <Test/>
+    <h1>prk with react</h1>
+    <h2>Counter value :{counter} </h2>
+    <button onClick={addValue}>add count{counter}</button>
+    <button onClick={removeValue}>remove count{counter}</button> 
     </>
     
-  );
+  ); 
 }
 
 export default App;
